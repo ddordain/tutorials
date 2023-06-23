a9ebeebceab822435312422a4ef89e35807a7382
ddordain <ddordain@student.42.fr>
2023-06-23 19:29:36 +0200

    [ADD] todo props

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 
 export class Todo extends Component {
     static template = "owl_playground.todo";
-
+    
+    static props = {
+      id: Number,
+      description: String, 
+      done: Boolean,
+    };
 
     setup() {
     this.todo = {id: 3, description: "buy milk", done: false};
